1f23e3e7dbc6559eab963b7356fd6a65fa3a01d7
Artyom Yagovdik <qweirdy@gmail.com>
2017-02-25 18:18:13 +0600

    Added: content script to remove logo and search; extension data

---
+++ README.md
@@ -0,0 +1,2 @@
+# plain-new-tab
+Simple Chrome extension to remove search and logo from the New tab.
---
+++ content.js
@@ -0,0 +1,4 @@
+let logo   = document.getElementById('lga'),
+    search = document.getElementById('f');
+logo.style.display = 'none';
+search.parentNode.removeChild(search);
